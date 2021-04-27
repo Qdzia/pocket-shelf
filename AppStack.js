884457298d9e76bd5fcd2547e2fe0ddb464eabf4
Qdzia <46885229+Qdzia@users.noreply.github.com>
2021-04-27 20:04:18 +0200

    Customize header

--- AppStack.js
+++ AppStack.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import Loading from './scenes/Loading'
-import Core from './scenes/Core'
-import Title from './scenes/Title'
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Loading from "./scenes/Loading";
+import Core from "./scenes/Core";
+import Title from "./scenes/Title";
+import Header from "./components/atoms/Header";
 
 const Stack = createStackNavigator();
 
 export default function App() {
-    return (
-        <Stack.Navigator>
-          <Stack.Screen name="Loading" component={Loading} />
-          <Stack.Screen name="Core" component={Core} />
-          <Stack.Screen name="Title" component={Title} />
-        </Stack.Navigator>
-    );
-  }
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Loading"
+        component={Loading}
+        options={{ headerTitle: (props) => <Header /> }}
+      />
+      <Stack.Screen
+        name="Core"
+        component={Core}
+        options={{
+          headerTitle: (props) => <Header />,
+          headerLeft: false
+        }}
+      />
+      <Stack.Screen
+        name="Title"
+        component={Title}
+        options={{ headerTitle: (props) => <Header /> }}
+      />
+    </Stack.Navigator>
+  );
+}
---
+++ components/atoms/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text, View, Button, StyleSheet } from "react-native";
+import Logo from '../../assets/Logo'
+
+export default Header = () => {
+  return (
+    <View style={styles.container}>
+        <Logo/>
+    
+      <Text style={styles.text}>Pocket Shelf</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: 'row'
+  },
+  text: {
+      marginHorizontal: 10,
+      fontWeight: 'bold',
+      fontSize: 18
+  }
+});
