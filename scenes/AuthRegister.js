f755d3d6d886f2657f028e03f3f3104185f142c7
Qdzia <46885229+Qdzia@users.noreply.github.com>
2021-05-12 13:35:35 +0200

    Add Register

--- scenes/AuthRegister.js
+++ scenes/AuthRegister.js
@@ -4,9 +4,33 @@ import * as firebase from "firebase";
 import PasswordInput from "../components/atoms/PasswordInput";
 
 export default function AuthRegister({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMassage, setErrorMassage] = useState("");
+
+  const register = () => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        navigation.navigate("Hub");
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMassage(error.message);
+      });
+  };
   return (
     <View style={styles.container}>
-      <Text>Register Hello</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="Email..."
+      />
+      <PasswordInput onChangeText={setPassword} value={password} />
+      <Text style={styles.error}>{errorMassage}</Text>
+      <Button style={styles.button} title="Create Account" onPress={register} />
     </View>
   );
 }
